d06b8bf296201600530a507b692edbe007bf594c
Enio Oliveira <eonascimetno@latam.stefanini.com>
2020-07-20 23:00:41 -0300

    Add project files.

---
+++ src/GestaoFornecedores.Business/Models/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestaoFornecedores.Business.Models
+{
+    class Class1
+    {
+    }
+}
---
+++ src/GestaoFornecedores.Business/Models/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoFornecedores.Business.Models
+{
+    public abstract class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+    }
+}
