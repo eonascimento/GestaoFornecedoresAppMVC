d033152ee10b021b2c6496e032ed816ebb01c6db
Enio Oliveira <eonascimetno@latam.stefanini.com>
2020-07-24 16:52:30 -0300

    Correção view model produto

--- src/GestaoFornecedores.App/ViewModels/ProdutoVIewModel.cs
+++ src/GestaoFornecedores.App/ViewModels/ProdutoVIewModel.cs
@@ -12,9 +12,14 @@ namespace GestaoFornecedores.App.ViewModels
     {
         [Key]
         public Guid Id { get; set; }
+
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(200, ErrorMessage ="O campo {0} precisa ter entre {2} 2 {1} caracteres", MinimumLength = 2)]
         public string Nome { get; set; }
+
+        [DisplayName("Forncedor")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public Guid FornecedorId { get; set; }
 
         [DisplayName("Descrição")]
         [Required(ErrorMessage ="O campo {0} é obrigatório")]
