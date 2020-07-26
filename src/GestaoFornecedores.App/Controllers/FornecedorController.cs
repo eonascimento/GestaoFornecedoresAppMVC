354b19e1f8e805e1f549488e564d657f56f5ceb1
Enio Oliveira <eonascimetno@latam.stefanini.com>
2020-07-25 22:52:26 -0300

    Criação controller Produto e Fornecedor

---
+++ src/GestaoFornecedores.App/Controllers/FornecedorController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using GestaoFornecedores.App.ViewModels;
+using GestaoFornecedores.Business.Models;
+using GestaoFornecedores.Business.Respositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoFornecedores.App.Controllers
+{
+    public class FornecedorController : BaseController
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IMapper _mapper;
+
+        public FornecedorController(IFornecedorRepository fornecedorRepository, IMapper mapper)
+        {
+            _fornecedorRepository = fornecedorRepository;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(_mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos()));
+        }
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var fornecedorViewModel = await ObterFornecedorEndereco(id);
+            if (fornecedorViewModel == null)
+            {
+                return NotFound();
+            }
+            return View(fornecedorViewModel);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(FornecedorViewModel fornecedorViewModel)
+        {
+            if (!ModelState.IsValid) return View(fornecedorViewModel);
+            var fornecedor = _mapper.Map<Fornecedor>(fornecedorViewModel);
+            await _fornecedorRepository.Adicionar(fornecedor);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var fornecedorViewModel = await ObterFornecedorProdutoEndereco(id);
+            if (fornecedorViewModel == null)
+            {
+                return NotFound();
+            }
+            return View(fornecedorViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, FornecedorViewModel fornecedorViewModel)
+        {
+            if (id != fornecedorViewModel.Id) return NotFound();
+            if (!ModelState.IsValid) return View(fornecedorViewModel);
+            var fornecedor = _mapper.Map<Fornecedor>(fornecedorViewModel);
+            await _fornecedorRepository.Atualizar(fornecedor);
+            return RedirectToAction("Index");
+        }
+
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var fornecedorViewModel = await ObterFornecedorEndereco(id);
+            if (fornecedorViewModel == null)
+            {
+                return NotFound();
+            }
+            return View(fornecedorViewModel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var fornecedorViewModel = await ObterFornecedorEndereco(id);
+            if (fornecedorViewModel == null)
+            {
+                return NotFound();
+            }
+            await _fornecedorRepository.Remover(id);
+            return RedirectToAction("Index");
+        }
+
+        private async Task<FornecedorViewModel> ObterFornecedorEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorEndereco(id));
+        }
+
+        private async Task<FornecedorViewModel> ObterFornecedorProdutoEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorProdutosEnderecos(id));
+        }
+    }
+}
---
+++ src/GestaoFornecedores.App/Controllers/ProdutoController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GestaoFornecedores.Business.Models;
+using GestaoFornecedores.Business.Respositories;
+using GestaoFornecedores.App.ViewModels;
+using AutoMapper;
+
+namespace GestaoFornecedores.App.Controllers
+{
+    public class ProdutoController : BaseController
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IMapper _mapper;
+
+        public ProdutoController(IProdutoRepository produtoRepository,
+                                 IFornecedorRepository fornecedorRepository,
+                                 IMapper mapper)
+        {
+            _produtoRepository = produtoRepository;
+            _fornecedorRepository = fornecedorRepository;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(_mapper.Map<IEnumerable<ProdutoViewModel  >>(await _produtoRepository.ObterProdutosForncedores()));
+        }
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var produtoViewModeel = await ObterProduto(id);
+            if (produtoViewModeel == null)
+            {
+                return NotFound();
+            }
+            return View(produtoViewModeel);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var produtoViewModel = await PopularFornecedores(new ProdutoViewModel());
+            return View(produtoViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProdutoViewModel produtoViewModel)
+        {
+            produtoViewModel = await PopularFornecedores(produtoViewModel);
+            if (ModelState.IsValid) return View(produtoViewModel);
+            await _produtoRepository.Adicionar(_mapper.Map<Produto>(produtoViewModel));
+            return View(produtoViewModel);
+        }
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var produtoViewModeel = await ObterProduto(id);
+            if (produtoViewModeel == null)
+            {
+                return NotFound();
+            }
+            return View(produtoViewModeel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, ProdutoViewModel produtoViewModel)
+        {
+            if (id != produtoViewModel.Id) return NotFound();
+            if (!ModelState.IsValid) return View(produtoViewModel);
+            await _produtoRepository.Atualizar(_mapper.Map<Produto>(produtoViewModel));
+            return RedirectToAction("Index");
+        }
+
+        // GET: Produtos/Delete/5
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var produto = await ObterProduto(id);
+            if (produto == null) return NotFound();
+            return View(produto);
+        }
+
+        // POST: Produtos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var produto = await ObterProduto(id);
+            if (produto == null) return NotFound();
+            await _produtoRepository.Remover(id);
+            return RedirectToAction("Index");
+        }
+
+        private async Task<ProdutoViewModel> ObterProduto(Guid id)
+        {
+            var produto = _mapper.Map<ProdutoViewModel>(await _produtoRepository.ObterProdutoFornecedor(id));
+            produto.Fornecedores = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
+            return produto;
+        }
+
+        private async Task<ProdutoViewModel> PopularFornecedores(ProdutoViewModel produtoVIewModel)
+        {
+            produtoVIewModel.Fornecedores = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
+            return produtoVIewModel;
+        }
+    }
+}
