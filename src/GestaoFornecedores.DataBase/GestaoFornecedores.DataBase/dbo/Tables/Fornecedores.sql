ccd265fd8ffab872a670e9d5fa041e81dea60ffc
Enio Oliveira <eonascimetno@latam.stefanini.com>
2020-08-24 19:22:20 -0300

    Importação BD gestaofornecedores

---
+++ src/GestaoFornecedores.DataBase/GestaoFornecedores.DataBase/dbo/Tables/Fornecedores.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Fornecedores] (
+    [Id]             UNIQUEIDENTIFIER NOT NULL,
+    [Nome]           VARCHAR (200)    NOT NULL,
+    [Documento]      VARCHAR (14)     NOT NULL,
+    [TipoFornecedor] INT              NOT NULL,
+    [Ativo]          BIT              NOT NULL,
+    CONSTRAINT [PK_Fornecedores] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ src/GestaoFornecedores.DataBase/GestaoFornecedores.DataBase/dbo/Tables/Produtos.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Produtos] (
+    [Id]           UNIQUEIDENTIFIER NOT NULL,
+    [FornecedorId] UNIQUEIDENTIFIER NOT NULL,
+    [Nome]         VARCHAR (200)    NOT NULL,
+    [Descricao]    VARCHAR (200)    NOT NULL,
+    [Imagem]       VARCHAR (200)    NOT NULL,
+    [Valor]        DECIMAL (18, 2)  NOT NULL,
+    [DataCadastro] DATETIME2 (7)    NOT NULL,
+    [Ativo]        BIT              NOT NULL,
+    CONSTRAINT [PK_Produtos] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Produtos_Fornecedores_FornecedorId] FOREIGN KEY ([FornecedorId]) REFERENCES [dbo].[Fornecedores] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Produtos_FornecedorId]
+    ON [dbo].[Produtos]([FornecedorId] ASC);
+
