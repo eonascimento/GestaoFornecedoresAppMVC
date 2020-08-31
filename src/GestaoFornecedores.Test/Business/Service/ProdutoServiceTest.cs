43c513d4569eb19c64fec5eb0ae2a21173ff034b
Enio Oliveira <eonascimetno@latam.stefanini.com>
2020-08-30 23:01:14 -0300

    Para testar pipeline dovops

---
+++ src/GestaoFornecedores.Test/Business/Service/ProdutoServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GestaoFornecedores.Test.Business.Service
+{
+    public class ProdutoServiceTest
+    {
+        [Fact]
+        public void Produto_Adicionar_TesteDevops()
+        {
+            Assert.Equal(4, 4);
+        }
+    }
+}
